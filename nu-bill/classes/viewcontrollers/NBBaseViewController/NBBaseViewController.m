48668b4e9b98453496926f59cee1b2b3996ed21b
Diego Chohfi <dchohfi@gmail.com>
2015-06-24 21:13:42 -0300

    Adding view controllers

---
+++ nu-bill/classes/viewcontrollers/NBBaseViewController/NBBaseViewController.m
@@ -0,0 +1,13 @@
+//
+//  NBBaseViewController.m
+//  nu-bill
+//
+//  Created by Diego Chohfi on 6/24/15.
+//  Copyright (c) 2015 nubank. All rights reserved.
+//
+
+#import "NBBaseViewController.h"
+
+@implementation NBBaseViewController
+
+@end
---
+++ nu-bill/classes/viewcontrollers/NBBillsViewController/NBBillsViewController.m
@@ -0,0 +1,13 @@
+//
+//  NBBillsViewController.m
+//  nu-bill
+//
+//  Created by Diego Chohfi on 6/24/15.
+//  Copyright (c) 2015 nubank. All rights reserved.
+//
+
+#import "NBBillsViewController.h"
+
+@implementation NBBillsViewController
+
+@end
